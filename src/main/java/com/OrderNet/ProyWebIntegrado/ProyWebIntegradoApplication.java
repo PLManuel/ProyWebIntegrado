f360ffa547b29c32482b56539dda64f19566a7d9
PLManuel <manuel715pl@gmail.com>
2025-05-27 18:44:37 -0500

    initial commit

---
+++ src/main/java/com/OrderNet/ProyWebIntegrado/ProyWebIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.OrderNet.ProyWebIntegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyWebIntegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyWebIntegradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OrderNet/ProyWebIntegrado/ProyWebIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrderNet.ProyWebIntegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyWebIntegradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
